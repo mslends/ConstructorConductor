38e5ae8971888f216865d166447f4c77e7bebbf6
Mike Spencer <mslends@gmail.com>
2016-02-10 19:55:46 -0700

    finalized changes

--- arrayProperty.js
+++ arrayProperty.js
@@ -1,5 +1,17 @@
 //Just like you can add methods to your own constructor, you can also add methods and properties to built in classes in JavaScript like Arrays and Objects.
 
 //Add a reverse method to the String 'class' so that every instance of String can call reverse and reverse itself.
-  
+
   //code here
+
+  var myString = "Hello World";
+  function reverseString (str) {
+    str = str.split("");
+  //  console.log(str);
+    str.reverse();
+  //  console.log(str);
+    str = str.join("");
+  //  console.log(str);
+    return str;
+  }
+  reverseString(myString);
--- sayName.js
+++ sayName.js
@@ -1,13 +1,21 @@
 //Create a Person constructor that accepts name and age as parameters and sets those properties accordingly in the Constructor.
 
   //code here
-
+  var Person = function (name, age){
+    this.name = name;
+    this.age = age;
+  };
 
 //Now create three instances of Person with data you make up
 
   //code here
-
+var mike =  new Person("Mike",  31);
+var jon =   new Person("Jon",   15);
+var steve = new Person("Steve", 67);
 
 //Now add a sayName method on your Person class that will alert the name of whatever Person instance called it.
 
   //code here
+  Person.prototype.sayName = function() {
+    alert(this.name);
+  }
